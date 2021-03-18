638e2cb54b11fbdc8da1a7663ca45fd8e66593a1
JonathanIlori <63069968+JonathanIlori@users.noreply.github.com>
2021-03-18 13:52:07 -0600

    Added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify, Response
+import requests
+import hashlib
+
+app = Flask(__name__)
+
+@app.route('/md5/<str:talk>')
+           def info(talk):
+            result = hashlib.md5(talk.encode())
+            final = result.hexdigest()
+           return f"The hexadecimal equivalent of hash is: {final}"
+
+@app.route('/fibonacci/<int:val>')
+           def term(val):
+           Out = 0
+           Sequence = [0,1]
 
