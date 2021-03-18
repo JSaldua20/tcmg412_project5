8f4b650f08fd3dfb5a1aaa074c87fdb55064b007
JSaldua20 <62914172+JSaldua20@users.noreply.github.com>
2021-03-18 15:27:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ app = Flask(__name__)
            Out = 0
            Sequence = [0,1]
 
+           if val > 0:
+            while Out < val:
+              Out = Sequence[-1] + Sequence[-2]
+              if (Out < val):
+                Sequence.append(Out)
+            return f"The result is: {Sequence}."
+           elif val <= 0:
+            return f"That is not a valid number"
+  
+@app.route('/factorial/<int:x>')
+           def bit(x):
+                      output = 1
+                      if x < 0:
+                                 return('You must provide a positive number')
+                      else:
+                                 while x > 0:
+                                            output = output * x
+                                            x = x - 1
+                                 return(output)
